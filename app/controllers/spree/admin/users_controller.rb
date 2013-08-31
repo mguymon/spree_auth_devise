8b3766fdc250d161430f3ae53a62223c3d011520
Michael Guymon <michael@tobedevoured.com>
2013-08-30 20:19:42 -0400

    use Spree.user_class.table_name for table name

--- app/controllers/spree/admin/users_controller.rb
+++ app/controllers/spree/admin/users_controller.rb
@@ -79,7 +79,7 @@ module Spree
             #disabling proper nested include here due to rails 3.1 bug
             #@collection = User.includes(:bill_address => [:state, :country], :ship_address => [:state, :country]).
             @collection = Spree.user_class.includes(:bill_address, :ship_address)
-                              .where("spree_users.email #{LIKE} :search
+                              .where("#{Spree.user_class.table_name}.email #{LIKE} :search
                                      OR (spree_addresses.firstname #{LIKE} :search AND spree_addresses.id = spree_users.bill_address_id)
                                      OR (spree_addresses.lastname  #{LIKE} :search AND spree_addresses.id = spree_users.bill_address_id)
                                      OR (spree_addresses.firstname #{LIKE} :search AND spree_addresses.id = spree_users.ship_address_id)
